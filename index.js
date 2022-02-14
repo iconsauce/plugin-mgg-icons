f5fc145db6e7927d72ec3c254f8d95aa15e0b1df
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-14 17:04:34 +0100

    fix: update plugin with core rules

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const path = require('path')
+const dirname = require('path').dirname
+
 module.exports = {
   prefix: 'mgg',
   regex: {
-    code: /(mgg)(\/{1}[a-z\-]+){1,2}/gm,
-    lib: /([@[a-zA-Z_\-\/]+\/([0-9a-zA-Z_-]+)\.svg)/,
+    code: /(mgg)(\/{1}[a-z-]+){1,2}/gm,
+    lib: /([@[a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\.svg)/,
   },
   selector: path => `mgg/${path[2].replace(/[_]+/g, '-')}`,
-  path: `${path.dirname(require.resolve('@maggioli-design-system/svg-icons/package.json'))}/**/*.svg`,
+  path: `${dirname(require.resolve('@maggioli-design-system/svg-icons/package.json'))}/**/*.svg`,
 }
