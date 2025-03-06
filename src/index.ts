eb91da53ef46e571754e5ead57e548384957e349
Nicola Tamburini <nicola.tamburini@maggioli.it>
2025-03-06 16:42:56 +0100

    export as esm module

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { IconsaucePlugin } from '@iconsauce/plugin'
 const maggioliSvgIconsPlugin: IconsaucePlugin = {
   prefix: 'mgg',
   regex: {
-    code: /(mgg)(\/{1}[0-9a-z-]+){1}/gm,
+    code: /(mgg)(\/[0-9a-z-]+)/gm,
     lib: /([a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\.svg)/,
   },
-  selector: (path: RegExpMatchArray) => `mgg/${path[2].replace(/[_]+/g, '-')}`,
+  selector: (path: RegExpMatchArray) => `mgg/${path[2].replace(/_+/g, '-')}`,
   path: `${resolve(__dirname, '../../../@maggioli-design-system/svg-icons')}/**/*.svg`,
 }
 
